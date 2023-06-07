663ffdfdb9a7b8637649e5ceca38696114f51992
bairagikumkum121 <bairagikumkum4@gmail.com>
2023-06-07 04:42:29 -1200

    java File

---
+++ SwapToNumber.java
@@ -0,0 +1,14 @@
+//programme for swapping to varible or number .
+public class SwapToNumber {
+
+    public static void main(String args[]) {
+        int num1 = 20;
+        int num2 = 10;
+        int num3;
+        System.out.println("Before Swapping A is:" + num1 + " and B is" + num2);
+        num3 = num1;
+        num1 = num2;
+        num2 = num3;
+        System.out.println("Affter Swapping A is:" + num1 + " and B is" + num2);
+    }
+}
